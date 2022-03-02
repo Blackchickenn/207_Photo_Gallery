ebff4192539a66de31b871fac82d0c256acf47a4
Blackchickenn <marcel.madaj@gmail.com>
2022-03-02 08:55:59 +0100

    finished gallery app

--- main.js
+++ main.js
@@ -5,15 +5,31 @@ async function getPhotos(){
     return photos
      
 }
+
+//function to creat my-photos component
+function getMyPhotosHtml(photos) {
+    let myPhotosHtml = photos.map(photo => {
+        return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
+    }).join('')
+    return `<div class="my-photos">${myPhotosHtml}</div>`
+}
+
 //render all photos from .json file
 getPhotos().then(photos => {
-    let myPhotosHtml = photos.map(photo =>{
-        return `<img src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
-    }).join("")
-    console.log(myPhotosHtml)
-//create a div with all photos inside
-    document.body.innerHTML = `
-    <div class="my-photos">${myPhotosHtml}</div>
-    `
+    let myPhotosHtml = getMyPhotosHtml(photos)
+    document.body.innerHTML = `<div class="my-gallery">
+        <img id="my-selected-photo" class="my-photo"
+        src="https://picsum.photos/id/${photos[2].id}1/300/300" />
+        ${myPhotosHtml}
+    </div>`
+    
+    let myPhotoImgs = Array.from(document.getElementsByClassName("my-photo"))
+    myPhotoImgs.forEach(photoImg => {
+        photoImg.addEventListener("click", e => {
+            let selectedPhotoSrc = photoImg.src.substr(0, photoImg.src.length - 7) + `300/300`
+            let selectedPhoto = document.getElementById("my-selected-photo")
+            selectedPhoto.src = selectedPhotoSrc
+            selectedPhoto.style.display = "inline"
+        })
+    })    
 })
-
